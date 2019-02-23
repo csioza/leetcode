744cde25fb9df9211291acb1650206ec96a1999e
Darren <csioza@163.com>
2019-02-23 16:15:23 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -4,7 +4,26 @@
 #include <vector>
 using namespace std;
 
-vector<int> twoSum(vector<int>& nums, int target) {
+//1. 两数之和
+vector<int> twoSum1(vector<int>& nums, int target) {
+    int len = nums.size();
+    vector<int> rlt;
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = len - 1; j > i; j--)
+        {
+            int sum = nums[i] + nums[j];
+            if (sum == target)
+            {
+                rlt.push_back(i);
+                rlt.push_back(j);
+                return rlt;
+            }
+        }
+    }
+    return rlt;
+}
+vector<int> twoSum2(vector<int>& nums, int target) {
     int len = nums.size();
     vector<int> rlt;
     map<int, int> keys;
@@ -23,33 +42,213 @@ vector<int> twoSum(vector<int>& nums, int target) {
             keys[nums[i]] = i;
         }
     }
-    //for(int i = 0; i < len; i++)
-    //{
-    //    for(int j = len - 1; j > i; j--)
-    //    {
-    //        int sum = nums[i] + nums[j];
-    //        if (sum == target)
-    //        {
-    //            rlt.push_back(i);
-    //            rlt.push_back(j);
-    //            return rlt;
-    //        }
-    //    }
-    //}
     return rlt;
 }
-
-int main()
-{
-    vector<int> nums;
-    nums.push_back(3);
-    nums.push_back(2);
-    nums.push_back(4);
-    vector<int> nums2;
-    nums2 = twoSum(nums,6);
-    if (nums2.size() == 2)
+//2. 两数相加
+ //Definition for singly - linked list.
+ struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+     ListNode* head = l1;
+     ListNode* hight = l2;
+     ListNode* hight2 = NULL;
+     int jinwei = 0;
+     while (l1)
+     {
+         if (l1->next == NULL)
+         {
+             if (l2 && l2->next)
+             {
+                 l1->next = l2->next;
+                 l2->next = NULL;
+             }
+             else
+             {
+                 hight2 = l1;
+             }
+         }
+         if (l1 && l2)
+         {
+             l1->val += (l2->val + jinwei);
+             if (l1->val)
+             {
+                 jinwei = l1->val / 10;
+                 l1->val %= 10;
+             }
+             else
+             {
+                 jinwei = 0;
+             }
+             l1 = l1->next;
+             l2 = l2->next;
+         }
+         else if (l1)
+         {
+             if (jinwei > 0)
+             {
+                 l1->val += jinwei;
+                 jinwei = l1->val / 10;
+                 l1->val %= 10;
+             }
+             l1 = l1->next;
+         }
+     }
+     if (jinwei > 0 && hight2)
+     {
+         hight->val = jinwei;
+         hight->next = NULL;
+         hight2->next = hight;
+     }
+     return head;
+ }
+ListNode* addTwoNumbers2(ListNode* l1, ListNode* l2) {
+    ListNode* head = l1;
+    ListNode* hight = l2;
+    int jinwei = 0;
+    while (l1)
     {
-        printf("%d,%d", nums2[0], nums2[1]);
+        if (l2)
+        {
+            l1->val += (l2->val + jinwei);
+            l2 = l2->next;
+            if (l1->next == NULL)
+            {
+                l1->next = l2;
+                l2 = NULL;
+            }
+        }
+        else
+        {
+            l1->val += jinwei;
+        }
+        if (l1->val >= 10)
+        {
+            jinwei = l1->val / 10;
+            l1->val %= 10;
+        }
+        else
+        {
+            jinwei = 0;
+        }
+        if (l1->next == NULL)
+        {
+            break;
+        }
+        l1 = l1->next;
     }
+    if (jinwei > 0)
+    {
+        hight->val = jinwei;
+        hight->next = NULL;
+        l1->next = hight;
+    }
+    return head;
+}
+int main2()
+{
+    ListNode * l1 = new ListNode(0);
+    l1->next = NULL;
+    ListNode * l2 = new ListNode(7);
+    ListNode * node3 = new ListNode(3);
+    l2->next = node3;
+    node3->next = NULL;
+    ListNode * rlt = addTwoNumbers(l1, l2);
+    printf("%d,%d", rlt->val, rlt->next->val);
     getchar();
+    return 0;
+}
+//3. 无重复字符的最长子串
+int lengthOfLongestSubstring(string s) {//错误答案
+    const char* str = s.c_str();
+    int max = 1;
+    int cnt = 1;
+    int len = strlen(str);
+    if (len <= 0)
+    {
+        return 0;
+    }
+    map<char, int> keys;
+    keys[str[0]] = 0;
+    int j = 0;
+    for (int i = 0; i < len && j < len;)
+    {
+        if (j == 0)
+        {
+            j = i + 1;
+        }
+        for (; j < len; j++)
+        {
+            map<char, int>::iterator iter = keys.find(str[j]);
+            if (iter != keys.end())
+            {
+                if (iter->second >= i)
+                {
+                    cnt = j - i;
+                    i = iter->second + 1;
+                    keys[str[j]] = j;
+                    j++;
+                    break;
+                }
+            }
+            else
+            {
+                cnt++;
+            }
+            keys[str[j]] = j;
+        }
+        if (cnt > max)
+        {
+            max = cnt;
+        }
+    }
+    return max;
+}
+int lengthOfLongestSubstring2(string s) {
+    const char* str = s.c_str();
+    int max = 1;
+    int cnt = 1;
+    int len = strlen(str);
+    if (len <= 0)
+    {
+        return 0;
+    }
+    map<char, int> keys;
+    keys[str[0]] = 0;
+    for (int i = 0, j = i + 1; i < len && j < len; j++)
+    {
+        map<char, int>::iterator iter = keys.find(str[j]);
+        if (iter != keys.end())
+        {
+            if (iter->second >= i)
+            {
+                i = iter->second + 1;
+                cnt = j - i + 1;
+            }
+            else
+            {
+                cnt++;
+            }
+        }
+        else
+        {
+            cnt++;
+        }
+        keys[str[j]] = j;
+        if (cnt > max)
+        {
+            max = cnt;
+        }
+    }
+    return max;
+}
+int main3()
+{
+    string s = "abcabcbb";
+    int num = lengthOfLongestSubstring(s);
+    printf("%d", num);
+    getchar();
+    return 0;
 }
