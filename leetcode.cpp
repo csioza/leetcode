0021629ff0a055bad5627cb747b828abb6555a67
darren <csioza@163.com>
2019-02-26 16:25:52 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -1447,6 +1447,7 @@ int main88()
     return 0;
 }
 
+//100. ÏàÍ¬µÄÊ÷
 //Definition for a binary tree node.
 struct TreeNode {
     int val;
@@ -1455,5 +1456,17 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 bool isSameTree(TreeNode* p, TreeNode* q) {
-
+    if (p == NULL && q == NULL)
+    {
+        return true;
+    }
+    if (p == NULL || q == NULL)
+    {
+        return false;
+    }
+    if (p->val != q->val)
+    {
+        return false;
+    }
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
 }
