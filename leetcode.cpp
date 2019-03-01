a2f4c649cc48bae122b47d4bcdccf236e6c9aaa2
Darren <csioza@163.com>
2019-03-01 20:03:41 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -2238,3 +2238,22 @@ ListNode* removeElements(ListNode* head, int val) {
     }
     return head;
 }
+
+//204. 计数质数
+int countPrimes(int n) {
+    bool *isPrime = new bool[n];
+    for (int i = 2; i < n; i++) {
+        isPrime[i] = true;
+    }
+    for (int i = 2; i * i < n; i++) {
+        if (!isPrime[i]) continue;
+        for (int j = i * i; j < n; j += i) {
+            isPrime[j] = false;
+        }
+    }
+    int count = 0;
+    for (int i = 2; i < n; i++) {
+        if (isPrime[i]) count++;
+    }
+    return count;
+}
