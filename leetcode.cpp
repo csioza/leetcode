4a62d899e3dc37f48e047b9b1088109a89e68fe1
Darren <csioza@163.com>
2019-03-07 20:08:54 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -4341,3 +4341,36 @@ int main()
     int r = uniquePaths(3,2);
     return 0;
 }
+//63. ²»Í¬Â·¾¶ II
+int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    int n = obstacleGrid.size();
+    if (n <= 0)
+        return 0;
+    int m = obstacleGrid[0].size();
+    if (m <= 0)
+        return 0;
+    long long **dp = new long long*[n];
+    for (int i = 0; i < n; i++)
+        dp[i] = new long long[m];
+    dp[0][0] = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (obstacleGrid[i][j])
+            {
+                dp[i][j] = 0;
+                continue;
+            }
+            if (i > 0 && j > 0)
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+            else if (i > 0)
+                dp[i][j] = dp[i - 1][j];
+            else if (j > 0)
+                dp[i][j] = dp[i][j - 1];
+        }
+    }
+    int res = dp[n - 1][m - 1];
+    delete[] dp;
+    return res;
+}
