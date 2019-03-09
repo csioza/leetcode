df87b075ae14533f315d7e1a6d3b137e5d206625
Darren <csioza@163.com>
2019-03-09 18:57:22 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -4833,3 +4833,17 @@ int nthUglyNumber2(int n) {//网上找的
     }
     return ugly.back();
 }
+//279. 完全平方数
+int numSquares(int n) {//网上找的
+    vector<int> dp(n + 1, INT_MAX);
+    for (int i = 1; i * i <= n; i++)
+        dp[i * i] = 1;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j * j < i; j++)
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
+    return dp[n];
+}
+//300. 最长上升子序列
+int lengthOfLIS(vector<int>& nums) {
+
+}
