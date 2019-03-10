58ef74ef68981c1e301c00b1b0d7346e8765291d
Darren <csioza@163.com>
2019-03-10 14:03:32 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -4924,3 +4924,14 @@ public:
         return 0;
     }
 };
+//309. 最佳买卖股票时机含冷冻期
+int maxProfit(vector<int>& prices) {//网上找的
+    int buy = INT_MIN, pre_buy = 0, sell = 0, pre_sell = 0;
+    for (int price : prices) {
+        pre_buy = buy;
+        buy = max(pre_sell - price, pre_buy);
+        pre_sell = sell;
+        sell = max(pre_buy + price, pre_sell);
+    }
+    return sell;
+}
