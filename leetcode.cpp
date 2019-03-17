15d84354ebf8fcd0634a2beb22a307a5e8d29e9c
Darren <csioza@163.com>
2019-03-17 23:01:59 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -6951,3 +6951,18 @@ public:
         return maxNum;
     }
 };
+//669. ÐÞ¼ô¶þ²æËÑË÷Ê÷
+class Solution669 {
+public:
+    TreeNode* trimBST(TreeNode* root, int L, int R) {
+        if (root == NULL || L > R)
+            return root;
+        if (root->val > R)
+            return trimBST(root->left, L, R);
+        if (root->val < L)
+            return trimBST(root->right, L, R);
+        root->left = trimBST(root->left, L, R);
+        root->right = trimBST(root->right, L, R);
+        return root;
+    }
+};
