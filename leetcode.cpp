1779fa142f264a06e1ed71b375252f768f69c1b1
Darren <csioza@163.com>
2019-03-17 10:50:21 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -6620,3 +6620,22 @@ public:
         return s;
     }
 };
+//617. 合并二叉树
+class Solution617 {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (t1 && t2)
+        {
+            t1->val += t2->val;
+            t1->left = mergeTrees(t1->left, t2->left);
+            t1->right = mergeTrees(t1->right, t2->right);
+            delete t2;//要想到删除的节点 delete掉
+            return t1;
+        }
+        else if (t1)
+            return t1;
+        else if (t2)
+            return t2;
+        return NULL;
+    }
+};
