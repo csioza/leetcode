3bd2dd4250614225ea8d89bd2a67df0c40e9b3be
darren <csioza@163.com>
2019-03-26 17:22:25 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -8027,3 +8027,28 @@ public:
         return r;
     }
 };
+//338. 比特位计数
+class Solution338 {
+public:
+    vector<int> countBits(int num) {
+        vector<int> dp(num + 1,0);
+        int j = 1;
+        for (int i = 1; i <= num; ++i)
+        {
+            if (i >= j)
+            {
+                j <<= 1;
+            }
+            dp[i] = 1 + dp[i-(j>>1)];
+        }
+        return dp;
+    }
+    vector<int> countBits2(int num) {//网上找的
+        vector<int> dp(num + 1, 0);
+        for (int i = 0; i <= num; ++i)
+        {
+            dp[i] = dp[i&(i - 1)] + 1;
+        }
+        return dp;
+    }
+};
