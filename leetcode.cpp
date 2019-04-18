e7ac13b9173c774849695d4ea2f356e1c5678939
darren <csioza@163.com>
2019-04-18 14:47:31 +0800

    a

--- leetcode.cpp
+++ leetcode.cpp
@@ -11720,3 +11720,25 @@ int main316()
     //string ss = s.removeDuplicateLetters("eywdgenmcnzhztolafcfnirfpuxmfcenlppegrcalgxjlajxmphwidqqtrqnmmbssotoywfrtylm");
     return 0;
 }
+
+//2019.04.18
+
+//703. 数据流中的第K大元素
+class KthLargest {//网上
+public:
+    //定义放在前面了
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int size;
+    KthLargest(int k, vector<int> nums) {
+        size = k;
+        for (int i = 0; i < nums.size(); i++) {
+            pq.push(nums[i]);
+            if (pq.size() > k) pq.pop();
+        }
+    }
+    int add(int val) {
+        pq.push(val);
+        if (pq.size() > size) pq.pop();
+        return pq.top();
+    }
+};
