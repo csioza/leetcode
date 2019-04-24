95c8852b027e3d30692818b64e4d69bbff53bf70
darren <csioza@163.com>
2019-04-24 13:08:07 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -13,149 +13,154 @@
 #include <functional>//因为用了greater<int>()
 using namespace std;
 
-//1. 两数之和
-vector<int> twoSum1(vector<int>& nums, int target) {
-    int len = nums.size();
-    vector<int> rlt;
-    for (int i = 0; i < len; i++)
-    {
-        for (int j = len - 1; j > i; j--)
-        {
-            int sum = nums[i] + nums[j];
-            if (sum == target)
-            {
-                rlt.push_back(i);
-                rlt.push_back(j);
-                return rlt;
-            }
-        }
-    }
-    return rlt;
-}
-vector<int> twoSum2(vector<int>& nums, int target) {
-    int len = nums.size();
-    vector<int> rlt;
-    map<int, int> keys;
-    for (int i = 0; i < len; i++)
-    {
-        int sub = target - nums[i];
-        map<int, int>::iterator iter = keys.find(sub);
-        if (iter != keys.end())
-        {
-            rlt.push_back(iter->second);
-            rlt.push_back(i);
-            return rlt;
-        }
-        else
-        {
-            keys[nums[i]] = i;
-        }
-    }
-    return rlt;
-}
-//2. 两数相加
-//Definition for singly - linked list.
- struct ListNode {
+struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
 };
-ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-     ListNode* head = l1;
-     ListNode* hight = l2;
-     ListNode* hight2 = NULL;
-     int jinwei = 0;
-     while (l1)
-     {
-         if (l1->next == NULL)
-         {
-             if (l2 && l2->next)
-             {
-                 l1->next = l2->next;
-                 l2->next = NULL;
-             }
-             else
-             {
-                 hight2 = l1;
-             }
-         }
-         if (l1 && l2)
-         {
-             l1->val += (l2->val + jinwei);
-             if (l1->val)
-             {
-                 jinwei = l1->val / 10;
-                 l1->val %= 10;
-             }
-             else
-             {
-                 jinwei = 0;
-             }
-             l1 = l1->next;
-             l2 = l2->next;
-         }
-         else if (l1)
-         {
-             if (jinwei > 0)
-             {
-                 l1->val += jinwei;
-                 jinwei = l1->val / 10;
-                 l1->val %= 10;
-             }
-             l1 = l1->next;
-         }
-     }
-     if (jinwei > 0 && hight2)
-     {
-         hight->val = jinwei;
-         hight->next = NULL;
-         hight2->next = hight;
-     }
-     return head;
- }
-ListNode* addTwoNumbers2(ListNode* l1, ListNode* l2) {
-    ListNode* head = l1;
-    ListNode* hight = l2;
-    int jinwei = 0;
-    while (l1)
-    {
-        if (l2)
+//1. 两数之和
+class Solution1 {
+public:
+    vector<int> twoSum1(vector<int>& nums, int target) {
+        int len = nums.size();
+        vector<int> rlt;
+        for (int i = 0; i < len; i++)
         {
-            l1->val += (l2->val + jinwei);
-            l2 = l2->next;
-            if (l1->next == NULL)
+            for (int j = len - 1; j > i; j--)
             {
-                l1->next = l2;
-                l2 = NULL;
+                int sum = nums[i] + nums[j];
+                if (sum == target)
+                {
+                    rlt.push_back(i);
+                    rlt.push_back(j);
+                    return rlt;
+                }
             }
         }
-        else
-        {
-            l1->val += jinwei;
-        }
-        if (l1->val >= 10)
-        {
-            jinwei = l1->val / 10;
-            l1->val %= 10;
-        }
-        else
-        {
-            jinwei = 0;
-        }
-        if (l1->next == NULL)
-        {
-            break;
-        }
-        l1 = l1->next;
+        return rlt;
     }
-    if (jinwei > 0)
-    {
-        hight->val = jinwei;
-        hight->next = NULL;
-        l1->next = hight;
+    vector<int> twoSum2(vector<int>& nums, int target) {
+        int len = nums.size();
+        vector<int> rlt;
+        map<int, int> keys;
+        for (int i = 0; i < len; i++)
+        {
+            int sub = target - nums[i];
+            map<int, int>::iterator iter = keys.find(sub);
+            if (iter != keys.end())
+            {
+                rlt.push_back(iter->second);
+                rlt.push_back(i);
+                return rlt;
+            }
+            else
+            {
+                keys[nums[i]] = i;
+            }
+        }
+        return rlt;
     }
-    return head;
-}
+};
+//2. 两数相加
+class Solution2 {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = l1;
+        ListNode* hight = l2;
+        ListNode* hight2 = NULL;
+        int jinwei = 0;
+        while (l1)
+        {
+            if (l1->next == NULL)
+            {
+                if (l2 && l2->next)
+                {
+                    l1->next = l2->next;
+                    l2->next = NULL;
+                }
+                else
+                {
+                    hight2 = l1;
+                }
+            }
+            if (l1 && l2)
+            {
+                l1->val += (l2->val + jinwei);
+                if (l1->val)
+                {
+                    jinwei = l1->val / 10;
+                    l1->val %= 10;
+                }
+                else
+                {
+                    jinwei = 0;
+                }
+                l1 = l1->next;
+                l2 = l2->next;
+            }
+            else if (l1)
+            {
+                if (jinwei > 0)
+                {
+                    l1->val += jinwei;
+                    jinwei = l1->val / 10;
+                    l1->val %= 10;
+                }
+                l1 = l1->next;
+            }
+        }
+        if (jinwei > 0 && hight2)
+        {
+            hight->val = jinwei;
+            hight->next = NULL;
+            hight2->next = hight;
+        }
+        return head;
+    }
+    ListNode* addTwoNumbers2(ListNode* l1, ListNode* l2) {
+        ListNode* head = l1;
+        ListNode* hight = l2;
+        int jinwei = 0;
+        while (l1)
+        {
+            if (l2)
+            {
+                l1->val += (l2->val + jinwei);
+                l2 = l2->next;
+                if (l1->next == NULL)
+                {
+                    l1->next = l2;
+                    l2 = NULL;
+                }
+            }
+            else
+            {
+                l1->val += jinwei;
+            }
+            if (l1->val >= 10)
+            {
+                jinwei = l1->val / 10;
+                l1->val %= 10;
+            }
+            else
+            {
+                jinwei = 0;
+            }
+            if (l1->next == NULL)
+            {
+                break;
+            }
+            l1 = l1->next;
+        }
+        if (jinwei > 0)
+        {
+            hight->val = jinwei;
+            hight->next = NULL;
+            l1->next = hight;
+        }
+        return head;
+    }
+};
 int main2()
 {
     ListNode * l1 = new ListNode(0);
@@ -164,114 +169,104 @@ int main2()
     ListNode * node3 = new ListNode(3);
     l2->next = node3;
     node3->next = NULL;
-    ListNode * rlt = addTwoNumbers(l1, l2);
+    Solution2 s;
+    ListNode * rlt = s.addTwoNumbers(l1, l2);
     printf("%d,%d", rlt->val, rlt->next->val);
-    getchar();
     return 0;
 }
 //3. 无重复字符的最长子串
-int lengthOfLongestSubstring(string s) {//错误答案
-    const char* str = s.c_str();
-    int max = 1;
-    int cnt = 1;
-    int len = strlen(str);
-    if (len <= 0)
-    {
-        return 0;
-    }
-    map<char, int> keys;
-    keys[str[0]] = 0;
-    int j = 0;
-    for (int i = 0; i < len && j < len;)
-    {
-        if (j == 0)
+class Solution3 {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int max = 1;
+        int cnt = 1;
+        int len = s.size();
+        if (len <= 0)
+            return 0;
+        map<char, int> keys;
+        keys[s[0]] = 0;
+        for (int i = 0, j = i + 1; i < len && j < len; j++)
         {
-            j = i + 1;
-        }
-        for (; j < len; j++)
-        {
-            map<char, int>::iterator iter = keys.find(str[j]);
+            map<char, int>::iterator iter = keys.find(s[j]);
             if (iter != keys.end())
             {
                 if (iter->second >= i)
                 {
-                    cnt = j - i;
                     i = iter->second + 1;
-                    keys[str[j]] = j;
-                    j++;
-                    break;
+                    cnt = j - i + 1;
                 }
+                else
+                    cnt++;
             }
             else
-            {
                 cnt++;
-            }
-            keys[str[j]] = j;
+            keys[s[j]] = j;
+            if (cnt > max)
+                max = cnt;
         }
-        if (cnt > max)
-        {
-            max = cnt;
-        }
+        return max;
     }
-    return max;
-}
-int lengthOfLongestSubstring2(string s) {
-    const char* str = s.c_str();
-    int max = 1;
-    int cnt = 1;
-    int len = strlen(str);
-    if (len <= 0)
-    {
-        return 0;
-    }
-    map<char, int> keys;
-    keys[str[0]] = 0;
-    for (int i = 0, j = i + 1; i < len && j < len; j++)
-    {
-        map<char, int>::iterator iter = keys.find(str[j]);
-        if (iter != keys.end())
-        {
-            if (iter->second >= i)
-            {
-                i = iter->second + 1;
-                cnt = j - i + 1;
-            }
-            else
-            {
-                cnt++;
-            }
-        }
-        else
-        {
-            cnt++;
-        }
-        keys[str[j]] = j;
-        if (cnt > max)
-        {
-            max = cnt;
-        }
-    }
-    return max;
-}
+};
 int main3()
 {
-    string s = "abcabcbb";
-    int num = lengthOfLongestSubstring(s);
+    Solution3 s;
+    string ss = "abcabcbb";
+    int num = s.lengthOfLongestSubstring(ss);
     printf("%d", num);
-    getchar();
     return 0;
 }
 //4. 寻找两个有序数组的中位数
-//double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-//
-//}
+class Solution4 {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        return 0;
+    }
+};
 //5. 最长回文子串
+class Solution5 {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+        int len = s.size();
+        if (len == 1) return s;
+        int longest = 1;
+        int start = 0;
+        vector<vector<int>> dp(len, vector<int>(len));
+        for (int i = 0; i < len; i++)
+        {
+            dp[i][i] = 1;
+            if (i < len - 1)
+            {
+                if (s[i] == s[i + 1])
+                {
+                    dp[i][i + 1] = 1;
+                    start = i;
+                    longest = 2;
+                }
+            }
+        }
+        for (int l = 3; l <= len; l++)//子串长度
+        {
+            for (int i = 0; i + l - 1 < len; i++)//枚举子串的起始点
+            {
+                int j = l + i - 1;//终点
+                if (s[i] == s[j] && dp[i + 1][j - 1] == 1)
+                {
+                    dp[i][j] = 1;
+                    start = i;
+                    longest = l;
+                }
+            }
+        }
+        return s.substr(start, longest);
+    }
+};
 int main5()
 {
-    string s = "abcabcbb";
-    int num = lengthOfLongestSubstring(s);
+    Solution5 s;
+    string ss = "abcabcbb";
+    int num = s.lengthOfLongestSubstring(ss);
     printf("%d", num);
-    getchar();
     return 0;
 }
 //6. Z 字形变换 
