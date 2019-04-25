2f66919c1c60ce3c7031c03d5f34fb79c6403ce5
darren <csioza@163.com>
2019-04-25 09:34:41 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -1839,38 +1839,41 @@ int main62()
     return 0;
 }
 //63. 不同路径 II
-int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-    int n = obstacleGrid.size();
-    if (n <= 0)
-        return 0;
-    int m = obstacleGrid[0].size();
-    if (m <= 0)
-        return 0;
-    long long **dp = new long long*[n];
-    for (int i = 0; i < n; i++)
-        dp[i] = new long long[m];
-    dp[0][0] = 1;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
+class Solution63 {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        if (n <= 0)
+            return 0;
+        int m = obstacleGrid[0].size();
+        if (m <= 0)
+            return 0;
+        long long **dp = new long long*[n];
+        for (int i = 0; i < n; i++)
+            dp[i] = new long long[m];
+        dp[0][0] = 1;
+        for (int i = 0; i < n; i++)
         {
-            if (obstacleGrid[i][j])
+            for (int j = 0; j < m; j++)
             {
-                dp[i][j] = 0;
-                continue;
+                if (obstacleGrid[i][j])
+                {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i > 0 && j > 0)
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+                else if (i > 0)
+                    dp[i][j] = dp[i - 1][j];
+                else if (j > 0)
+                    dp[i][j] = dp[i][j - 1];
             }
-            if (i > 0 && j > 0)
-                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
-            else if (i > 0)
-                dp[i][j] = dp[i - 1][j];
-            else if (j > 0)
-                dp[i][j] = dp[i][j - 1];
         }
+        int res = dp[n - 1][m - 1];
+        delete[] dp;
+        return res;
     }
-    int res = dp[n - 1][m - 1];
-    delete[] dp;
-    return res;
-}
+};
 //64. 最小路径和
 int minPathSum(vector<vector<int>>& grid) {
     int n = grid.size();
