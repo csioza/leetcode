8300d65dc58b05269b48cae180ad82c7c3f6bfa0
darren <csioza@163.com>
2019-05-07 09:59:35 +0800

    i

--- tool.cpp
+++ tool.cpp
@@ -1,8 +1,4 @@
-//vector<vector<int> > dp(n, vector<int>(m));
-//int* *dp = new int*[n];
-//for (int i = 0; i < n; i++)
-//    dp[i] = new int[m];
-//
+
 //#include <stdio.h>
 ////int demo(int *a)
 ////{
